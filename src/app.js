052873d6511517118fdae23719e36e6f08aee2ab
Daiana Michels <daiana.michels@pixeon.com>
2023-10-31 10:13:54 -0300

    added new route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,35 @@ import express from 'express';
 
 const app = express();
 
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954
+  }
+]
+
 app.get("/", (req, res) => {
   res.status(200).send("Node.js course");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
 });
 
 export default app;
