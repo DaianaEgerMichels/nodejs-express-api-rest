3d309e0db716335e48ad953cda48d5f0afb04df9
Daiana Michels <daiana.michels@pixeon.com>
2023-10-31 09:19:05 -0300

    added more routes

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ import http from 'http';
 // Port standard
 const PORT = 3000;
 
-// create a local server
+const routes = {
+  "/": "Node.js course - hello world :)",
+  "/books": "Welcome to the book store",
+  "/authors": "Authors",
+}
 
+// Create a local server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Node.js course , hello world :)');
+  res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
