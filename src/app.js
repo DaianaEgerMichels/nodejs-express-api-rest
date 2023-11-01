58581dca941b3338e7969dbad360eb44c46b21e0
Daiana Michels <daiana.michels@pixeon.com>
2023-11-01 14:13:57 -0300

    added get by id and put

--- src/app.js
+++ src/app.js
@@ -28,6 +28,10 @@ const books = [
   }
 ]
 
+function getBook(id) {
+  return books.findIndex(book => book.id === Number(id));
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Node.js course");
 });
@@ -36,9 +40,20 @@ app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+  const index = getBook(req.params.id);
+  res.status(200).json(books[index]);
+})
+
 app.post("/books", (req, res) => {
   books.push(req.body);
   res.status(201).send("Book created");
 });
+
+app.put("/books/:id", (req, res) => {
+  const index = getBook(req.params.id);
+  books[index].name = req.body.name;
+  res.status(200).json(books);
+})
 
 export default app;
